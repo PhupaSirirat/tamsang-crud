65db83976473706c8322c0c6fa1914c7e89f1c1a
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2022-12-11 01:15:50 +0700

    Login Page revert change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 import React from 'react';
+=======
+import React, { useState } from 'react';
+import Helmet from '../components/Helmet/helmet';
+// import CommonSection 
+>>>>>>> parent of 325339a (Deploy Fix)
 
 import { Container, Row, Col } from "reactstrap";
 import products from "../assets/data/products";
@@ -6,11 +12,11 @@ import NewProductCard from "../components/UI/newproduct-card";
 import '../styles/add-bootstrap.css';
 
 const Home = () => {
-    // const [allProducts, setAllProducts] = useState(products);
+    const [allProducts, setAllProducts] = useState(products);
     return (
         <Container>
             <Row className="row">
-                {products.map((item) => (
+                {allProducts.map((item) => (
                     <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mt-5">
                     <NewProductCard item={item}/>
                 </Col>
