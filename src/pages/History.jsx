294de93b84fdce65cffffd6a04c989b4db5c8199
KanbH <pohbnak@gmail.com>
2022-12-10 21:41:07 +0700

    History page

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -4,23 +4,31 @@ import Order from '../store/Order';
 import '../styles/history.css';
 
 const History = (props) => {
-    const {items} = props;
+    const { items } = props;
     // const cartItems = useSelector((state)=> state.cart.cartItems);
     return (
-        <div>
+        <div class="historyframe">
             {items.length === 0 ? (
                 <div className="no_purchase">
                     <h5 >No purchase history</h5>
                 </div>
             ) : (
                 <Container>
-                    <Row>
-                        <div className="order_history">
-                            {items.map((element)=>{
-                                return <Order {...element}/>
-                            })}
-                        </div>
-                    </Row>
+                    <table class="historytable">
+                        <tr class="headrow">
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Postal Code</th>
+                            <th>Phone</th>
+                            <th>Email</th>
+                            <th>Total</th>
+                            <th>Date</th>
+                            <th>Status</th>
+                        </tr>
+                                    {items.map((element) => {
+                                        return <Order {...element} />
+                                    })}
+                    </table>
                 </Container>
             )}
         </div>
--- src/store/Order.js
+++ src/store/Order.js
@@ -1,14 +1,25 @@
+import '../styles/history.css';
+import { useSelector, useDispatch } from "react-redux";
 const Order = (props) => {
     const {name, address, postalcode, phonenumber, email} = props;
     console.log(name, address, postalcode, phonenumber);
+    const totalAmount = useSelector((state) => state.cart.totalAmount);
+    const date = new Date();
+    const showTime = date.getFullYear() + "/" + (date.getMonth()+1) + "/" + date.getDate() 
+    + " " + date.getHours() 
+        + ":" + date.getMinutes() 
+        + ":" + date.getSeconds();
     return (
-        <div>
-            Name: {name}
-            Address: {address}
-            Postalcode: {postalcode}
-            Phone: {phonenumber}
-            Email: {email}
-        </div>
+        <tr class="datarow">
+            <td>{name}</td>
+            <td>{address}</td>
+            <td>{postalcode}</td>
+            <td>{phonenumber}</td>
+            <td>{email}</td>
+            <td>{totalAmount+30}฿</td>
+            <td>{showTime}</td>
+            <td>pending</td>
+        </tr>
     )
 }
 
