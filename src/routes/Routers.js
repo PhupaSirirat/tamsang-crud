39248ac7106238b47bd24b5b9df3555ea63a6ca0
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2022-12-11 01:30:46 +0700

    package Update

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -21,7 +21,7 @@ const Routers = () => {
     }
     return <Routes>
         <Route path='/' element={<Navigate to='/home'/>}/>
-        <Route path='/tamsang-crud' element={'/home'}/>
+        {/* <Route path='/tamsang-crud' element={<Home />}/> */}
         <Route path='/home' element={<Home />}/>
         <Route path='/cart' element={<Cart />}/>
         <Route path='/contact' element={<Contact />}/>
